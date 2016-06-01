9fae0fd932d4ecb97a8b02760ee711db08b24bf1
Grover Menacho <grover.menacho@poiesisconsulting.com>
2016-06-01 09:54:41 -0400

    website_product_category: [UPD] Categories with style

--- website_product_category/controllers/main.py
+++ website_product_category/controllers/main.py
@@ -6,7 +6,112 @@ from openerp.addons.web.http import request
 from openerp.addons.website_sale.controllers.main import website_sale
 from openerp.addons.website.models.website import slug
 
+PPG = 20 # Products Per Page
+PPR = 4  # Products Per Row
+
+class table_compute(object):
+    def __init__(self):
+        self.table = {}
+
+    def _check_place(self, posx, posy, sizex, sizey):
+        res = True
+        for y in range(sizey):
+            for x in range(sizex):
+                if posx+x>=PPR:
+                    res = False
+                    break
+                row = self.table.setdefault(posy+y, {})
+                if row.setdefault(posx+x) is not None:
+                    res = False
+                    break
+            for x in range(PPR):
+                self.table[posy+y].setdefault(x, None)
+        return res
+
+    def process(self, products, ppg=PPG):
+        # Compute products positions on the grid
+        minpos = 0
+        index = 0
+        maxy = 0
+        for p in products:
+            x = min(max(p.website_size_x, 1), PPR)
+            y = min(max(p.website_size_y, 1), PPR)
+            if index>=ppg:
+                x = y = 1
+
+            pos = minpos
+            while not self._check_place(pos%PPR, pos/PPR, x, y):
+                pos += 1
+            # if 21st products (index 20) and the last line is full (PPR products in it), break
+            # (pos + 1.0) / PPR is the line where the product would be inserted
+            # maxy is the number of existing lines
+            # + 1.0 is because pos begins at 0, thus pos 20 is actually the 21st block
+            # and to force python to not round the division operation
+            if index >= ppg and ((pos + 1.0) / PPR) > maxy:
+                break
+
+            if x==1 and y==1:   # simple heuristic for CPU optimization
+                minpos = pos/PPR
+
+            for y2 in range(y):
+                for x2 in range(x):
+                    self.table[(pos/PPR)+y2][(pos%PPR)+x2] = False
+            self.table[pos/PPR][pos%PPR] = {
+                'product': p, 'x':x, 'y': y,
+                'class': " ".join(map(lambda x: x.html_class or '', p.website_style_ids))
+            }
+            if index<=ppg:
+                maxy=max(maxy,y+(pos/PPR))
+            index += 1
+
+        # Format table according to HTML needs
+        rows = self.table.items()
+        rows.sort()
+        rows = map(lambda x: x[1], rows)
+        for col in range(len(rows)):
+            cols = rows[col].items()
+            cols.sort()
+            x += len(cols)
+            rows[col] = [c for c in map(lambda x: x[1], cols) if c != False]
+
+        return rows
+
+        # TODO keep with input type hidden
+
 class website_product_category(website_sale):
+    def _get_search_domain(self, search, category, attrib_values):
+        domain = request.website.sale_product_domain()
+        if search:
+            for srch in search.split(" "):
+                domain += [
+                    '|', '|', '|', ('name', 'ilike', srch), ('description', 'ilike', srch),
+                    ('description_sale', 'ilike', srch), ('product_variant_ids.default_code', 'ilike', srch)]
+
+        print "CATEGORY"
+        print category
+        if category:
+            domain += [('public_categ_ids', '=', int(category))]
+        else:
+            domain += [('public_categ_ids', '=', None)]
+
+        if attrib_values:
+            attrib = None
+            ids = []
+            for value in attrib_values:
+                if not attrib:
+                    attrib = value[0]
+                    ids.append(value[1])
+                elif value[0] == attrib:
+                    ids.append(value[1])
+                else:
+                    domain += [('attribute_line_ids.value_ids', 'in', ids)]
+                    attrib = value[0]
+                    ids = [value[1]]
+            if attrib:
+                domain += [('attribute_line_ids.value_ids', 'in', ids)]
+
+        return domain
+
     @http.route([
         '/shop',
         '/shop/page/<int:page>',
@@ -20,6 +125,17 @@ class website_product_category(website_sale):
 
         child_categ_ids = []
 
+        if ppg:
+            try:
+                ppg = int(ppg)
+            except ValueError:
+                ppg = PPG
+            post["ppg"] = ppg
+        else:
+            ppg = PPG
+
+        url = "/shop"
+
         if category:
 
             category = pool['product.public.category'].browse(cr, uid, int(category), context=context)
@@ -32,7 +148,16 @@ class website_product_category(website_sale):
             for c in categs:
                 child_categ_ids.append(c)
 
+        else:
+            category_obj = pool['product.public.category']
+            category_ids = category_obj.search(cr, uid, [('parent_id', '=', None)], context=context)
+            categs = category_obj.browse(cr, uid, category_ids, context=context)
+
+            for c in categs:
+                child_categ_ids.append(c)
+
         r.qcontext['child_categories'] = child_categ_ids
+
 
 
         return r
