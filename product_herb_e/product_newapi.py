8bcc3a2bd648915ac05e75f56b556fe9fc4001d3
Grover Menacho <grover.menacho@poiesisconsulting.com>
2016-06-08 09:21:04 -0400

    product_herb_e: [NEW] Overwrite

--- product_herb_e/product_newapi.py
+++ product_herb_e/product_newapi.py
@@ -20,21 +20,12 @@
 #
 ##############################################################################
 
-import time
 from openerp import models, fields, api, _
-from openerp.osv import osv
-import openerp.addons.decimal_precision as dp
-from openerp.exceptions import except_orm, Warning, RedirectWarning
 
-from lxml import etree
-from openerp import SUPERUSER_ID
-from datetime import datetime
-import openerp
-import math
 
 class ProductProduct(models.Model):
 
-    _name = 'product.product'
+    _inherit = 'product.product'
     _order = 'id desc'
 
     @api.one
