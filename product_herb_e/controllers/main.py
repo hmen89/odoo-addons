c9c8fc62478fce25aa9c37ca8b23a908dc137900
Grover Menacho <grover.menacho@poiesisconsulting.com>
2016-06-09 19:24:08 -0400

    product_herb_e: [NEW] Overwrite

--- product_herb_e/controllers/main.py
+++ product_herb_e/controllers/main.py
@@ -8,50 +8,39 @@ import openerp
 from openerp import http, SUPERUSER_ID
 from openerp.http import request
 from openerp.tools.translate import _
+from openerp.addons.web.controllers.main import ensure_db
 
 
 from openerp.addons.auth_signup.res_users import SignupError
 from openerp.addons.website_form.controllers.main import WebsiteForm
 from openerp.addons.website_sale.controllers.main import website_sale
-from openerp.addons.auth_signup.controllers.main import AuthSignupHome
 
 _logger = logging.getLogger(__name__)
 
-class SignUp(AuthSignupHome):
-    def _signup_with_values(self, token, values):
-        db, login, password = request.registry['res.users'].signup(request.cr, openerp.SUPERUSER_ID, values, token)
-        request.cr.commit()  # as authenticate will use its own cursor we need to commit the current transaction
-        #uid = request.session.authenticate(db, login, password)
-        #if not uid:
-        #    raise SignupError(_('Authentication Failed.'))
 
-    @http.route(['/web/signup','/page/website.getverified', '/page/getverified'], type='http', auth='public', website=True)
+class HerbSignupHome(openerp.addons.web.controllers.main.Home):
+
+    @http.route()
+    def web_login(self, *args, **kw):
+        ensure_db()
+        response = super(HerbSignupHome, self).web_login(*args, **kw)
+        response.qcontext.update(self.get_auth_signup_config())
+        if request.httprequest.method == 'GET' and request.session.uid and request.params.get('redirect'):
+            # Redirect if already logged in and redirect param is present
+            return http.redirect_with_hash(request.params.get('redirect'))
+        return response
+
+    @http.route(['/website/page/getverified','/page/getverified'], type='http', auth='public', website=True)
     def web_auth_signup(self, *args, **kw):
         qcontext = self.get_auth_signup_qcontext()
 
         if not qcontext.get('token') and not qcontext.get('signup_enabled'):
             raise werkzeug.exceptions.NotFound()
 
-        if 'reco_issuer' in qcontext and request.httprequest.method == 'POST':
-            if qcontext.get('partner_id'):
-                partner_id = int(qcontext.get('partner_id'))
-                del qcontext['partner_id']
-                try:
-                    request.env['res.partner'].sudo().browse(partner_id).write(qcontext)
-                    return self.contact_success(*args, **kw)
-                except (SignupError, AssertionError), e:
-                    _logger.error(e.message)
-                    qcontext['error'] = _("Could not register your data. Try again.")
-
-
-        if 'error' not in qcontext and 'reco_issuer' not in qcontext and request.httprequest.method == 'POST':
+        if 'error' not in qcontext and request.httprequest.method == 'POST':
             try:
                 self.do_signup(qcontext)
-                if qcontext.get('login'):
-                    partner_ids = request.env["res.partner"].sudo().search([('email', '=', qcontext.get('login'))]).write({'verified': False})
-
-                return self.contact(*args, **kw)
-                #return super(AuthSignupHome, self).web_login(*args, **kw)
+                return self.contact_success(*args, **kw)
             except (SignupError, AssertionError), e:
                 if request.env["res.users"].sudo().search([("login", "=", qcontext.get("login"))]):
                     qcontext["error"] = _("Another user is already registered using this email address.")
@@ -59,24 +48,52 @@ class SignUp(AuthSignupHome):
                     _logger.error(e.message)
                     qcontext['error'] = _("Could not create a new account.")
 
-        return request.render('auth_signup.signup', qcontext)
+        return request.render('website.contacts', qcontext)
 
-    def contact(self, *args, **kw):
+    def get_auth_signup_config(self):
+        """retrieve the module config (which features are enabled) for the login page"""
 
+        icp = request.registry.get('ir.config_parameter')
+        return {
+            'signup_enabled': icp.get_param(request.cr, openerp.SUPERUSER_ID, 'auth_signup.allow_uninvited') == 'True',
+            'reset_password_enabled': icp.get_param(request.cr, openerp.SUPERUSER_ID, 'auth_signup.reset_password') == 'True',
+        }
+
+    def get_auth_signup_qcontext(self):
+        """ Shared helper returning the rendering context for signup and reset password """
         qcontext = request.params.copy()
-        values = {}
-        partner_id = False
+        qcontext.update(self.get_auth_signup_config())
+        if qcontext.get('token'):
+            try:
+                # retrieve the user info (name, login or email) corresponding to a signup token
+                res_partner = request.registry.get('res.partner')
+                token_infos = res_partner.signup_retrieve_info(request.cr, openerp.SUPERUSER_ID, qcontext.get('token'))
+                for k, v in token_infos.items():
+                    qcontext.setdefault(k, v)
+            except:
+                qcontext['error'] = _("Invalid signup token")
+                qcontext['invalid_token'] = True
+        return qcontext
 
-        if kw.get('login'):
-            partner_ids = request.env["res.partner"].sudo().search([('email', '=', qcontext.get('login'))])
-            partner_id = partner_ids[0]
+    def do_signup(self, qcontext):
+        """ Shared helper that creates a res.partner out of a token """
+        values = dict((key, qcontext.get(key)) for key in ('login', 'name', 'password'))
+        partner_values = dict((key, qcontext.get(key)) for key in ('street', 'street2', 'city', 'zip', 'dob', 'phone', 'mobile', 'reco_patient_id', 'reco_issued', 'reco_expired', 'reco_issuer', 'reco_license', 'reco_verification_url'))
+        assert any([k for k in values.values()]), "The form was not properly filled in."
+        assert values.get('password') == qcontext.get('confirm_password'), "Passwords do not match; please retype them."
+        supported_langs = [lang['code'] for lang in request.registry['res.lang'].search_read(request.cr, openerp.SUPERUSER_ID, [], ['code'])]
+        if request.lang in supported_langs:
+            values['lang'] = request.lang
+        self._signup_with_values(qcontext.get('token'), values, partner_values)
+        request.cr.commit()
 
-        values['countries'] = request.env['res.country'].search([])
-        if qcontext and partner_id:
-            values['name'] = qcontext.get('name')
-            values['email_from'] = qcontext.get('login')
-            values['partner_id'] = str(partner_id.id)
-        return request.website.render("website.contacts", values)
+    def _signup_with_values(self, token, values, partner_values):
+        db, login, password = request.registry['res.users'].signup_herb(request.cr, openerp.SUPERUSER_ID, values, partner_values, token)
+        request.cr.commit()     # as authenticate will use its own cursor we need to commit the current transaction
+        #uid = request.session.authenticate(db, login, password)
+        #if not uid:
+        #    raise SignupError(_('Authentication Failed.'))
+
 
     @http.route(['/page/website.contacts_thanks', '/page/contacts_thanks'], type='http', auth="public", website=True)
     def contact_success(self, *args, **kw):
--- product_herb_e/partner.py
+++ product_herb_e/partner.py
@@ -35,4 +35,65 @@ class res_users(osv.osv):
         if not res2:
             raise openerp.exceptions.AccessDenied()
 
+
+    def signup_herb(self, cr, uid, values, partner_values, token=None, context=None):
+        """ signup a user, to either:
+            - create a new user (no token), or
+            - create a user for a partner (with token, but no user for partner), or
+            - change the password of a user (with token, and existing user).
+            :param values: a dictionary with field values that are written on user
+            :param token: signup token (optional)
+            :return: (dbname, login, password) for the signed up user
+        """
+        if token:
+            # signup with a token: find the corresponding partner id
+            res_partner = self.pool.get('res.partner')
+            partner = res_partner._signup_retrieve_partner(
+                cr, uid, token, check_validity=True, raise_exception=True, context=None)
+            # invalidate signup token
+            partner.write({'signup_token': False, 'signup_type': False, 'signup_expiration': False, 'verified': False})
+            partner.write(partner_values)
+
+            partner_user = partner.user_ids and partner.user_ids[0] or False
+
+            # avoid overwriting existing (presumably correct) values with geolocation data
+            if partner.country_id or partner.zip or partner.city:
+                values.pop('city', None)
+                values.pop('country_id', None)
+            if partner.lang:
+                values.pop('lang', None)
+
+            if partner_user:
+                # user exists, modify it according to values
+                values.pop('login', None)
+                values.pop('name', None)
+                partner_user.write(values)
+                return (cr.dbname, partner_user.login, values.get('password'))
+            else:
+                # user does not exist: sign up invited user
+                values.update({
+                    'name': partner.name,
+                    'partner_id': partner.id,
+                    'email': values.get('email') or values.get('login'),
+                })
+                if partner.company_id:
+                    values['company_id'] = partner.company_id.id
+                    values['company_ids'] = [(6, 0, [partner.company_id.id])]
+                self._signup_create_user(cr, uid, values, context=context)
+        else:
+            # no token, sign up an external user
+            values['email'] = values.get('email') or values.get('login')
+            self._signup_create_user(cr, uid, values, context=context)
+            res_partner = self.pool.get('res.partner')
+            res_partner_ids = res_partner.search(cr, uid, [('email','=',values.get('email') or values.get('login'))])
+            if res_partner_ids:
+                partner_id = res_partner.browse(cr, uid, res_partner_ids[0])
+                partner_id.write(
+                    {'verified': False})
+                partner_id.write(partner_values)
+
+
+
+        return (cr.dbname, values.get('login'), values.get('password'))
+
 res_users
